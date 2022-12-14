994677aea210183cc07761ac077a1366070e5042
Matthias Verbert <matthias@whitehare.be>
2022-12-14 10:03:44 +0100

    Day12 done

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -2,32 +2,51 @@ import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 import org.neo4j.driver.Query
 import org.neo4j.driver.Result
+import org.neo4j.driver.exceptions.NoSuchRecordException
 import org.testcontainers.containers.Neo4jContainer
 import org.testcontainers.utility.DockerImageName
 
 fun main() {
-    val grid = loadGrid("day12/test.input")
-
+    val grid = loadGrid("day12/real.input")
     val start = grid.find { it.isStart }!!
     val end = grid.find { it.isEnd }!!
 
-    val createStatements = grid.map { "(:Square {x: ${it.x}, y: ${it.y}, z: ${it.height}})" }.joinToString(", ", "CREATE ")
-    val listStatement = "MATCH (n:Square {x:4, y:3}) RETURN n"
-    val shortestPathStmt = "MATCH (from:Square {x:${start.x}, y:${start.y}}), (to:Square {x:${end.x}, y:${end.y}}), p = shortestPath((from)-[*]->(to)) RETURN p"
-
     val neo = startNeo()
+
+    // created indices
     writeInNeo("CREATE INDEX ON :Square(x)", neo.first) { println(it.consume()) }
     writeInNeo("CREATE INDEX ON :Square(y)", neo.first) { println(it.consume()) }
-    writeInNeo(createStatements, neo.first) {println(it.consume()) }
-    readFromNeo(listStatement, neo.first) { it.list().map { r -> r.toString() }}.forEach { println(it) }
+
+    // load all nodes
+    val createStatements = grid.map { "(:Square {x: ${it.x}, y: ${it.y}, z: ${it.height}})" }.joinToString(", ", "CREATE ")
+    writeInNeo(createStatements, neo.first) { println(it.consume()) }
+
+    // load all relations between nodes ("can travel from - to")
     grid.forEach { createRelationsFor(it, grid, neo) }
 
-    readFromNeo(shortestPathStmt, neo.first) {
-        val record = it.single()
-        val asPath = record.get("p").asPath()
-        asPath.forEach { println(it.relationship().type()) }
-        println(asPath.length())
+    // find all the shortest routes from the possible starting points
+    val shortest = grid.filter { it.height == start.height }.map { altStart ->
+        print("Evaluating $altStart")
+        val shortestPathStmt = "MATCH (from:Square {x:${altStart.x}, y:${altStart.y}}), (to:Square {x:${end.x}, y:${end.y}}), p = shortestPath((from)-[*]->(to)) RETURN p"
+        val length = readFromNeo(shortestPathStmt, neo.first) {
+            try {
+                it.single().get("p").asPath().length()
+            } catch (e: NoSuchRecordException) {
+                null
+            }
+        }
+        if (length != null) {
+            println(" -> $length")
+            Pair(altStart, length)
+        } else {
+            println(" -> impossible")
+            null
+        }
     }
+        .filterNotNull()
+        .minBy { it.second }
+
+    println("Shortest possible route from ${shortest.first} -> ${shortest.second}")
 
     neo.second.stop()
 
@@ -40,21 +59,21 @@ fun getNodeFor(x: Int, y: Int, grid: List<Node>): Node? {
 fun createRelationsFor(it: Node, grid: List<Node>, neo: Pair<Driver, Neo4jContainer<*>>) {
     // can I go up?
     createRelationshipFromTo(grid, neo, it.x, it.y, it.x, it.y - 1, it.height, "NORTH")
-
     // can I go down?
     createRelationshipFromTo(grid, neo, it.x, it.y, it.x, it.y + 1, it.height, "SOUTH")
-
     // can I go left?
     createRelationshipFromTo(grid, neo, it.x, it.y, it.x - 1, it.y, it.height, "WEST")
-
     // can I go right?
     createRelationshipFromTo(grid, neo, it.x, it.y, it.x + 1, it.y, it.height, "EAST")
+
+    println("Relations for $it created")
 }
 
 private fun createRelationshipFromTo(grid: List<Node>, neo: Pair<Driver, Neo4jContainer<*>>, fromX: Int, fromY: Int, toX: Int, toY: Int, fromHeight: Int, type: String) {
     val up = getNodeFor(toX, toY, grid)?.height
     if (up != null && up <= fromHeight + 1) {
-        writeInNeo("MATCH (from:Square {x: $fromX, y:$fromY}) WITH from MATCH (to:Square {x: $toX, y:$toY}) CREATE (from)-[:$type]->(to)", neo.first) { println(it.consume()) };
+        writeInNeo("MATCH (from:Square {x: $fromX, y:$fromY}) WITH from MATCH (to:Square {x: $toX, y:$toY}) CREATE (from)-[:$type]->(to)", neo.first) {
+        };
     }
 }
 
@@ -93,15 +112,14 @@ fun height(input: Char): Int {
     return input.code - 97
 }
 
-fun startNeo() : Pair<Driver, Neo4jContainer<*>> {
-
-    var neo4jContainer = Neo4jContainer(DockerImageName.parse("neo4j:4.4"))
+fun startNeo(): Pair<Driver, Neo4jContainer<*>> {
+    val neo4jContainer = Neo4jContainer(DockerImageName.parse("neo4j:4.4"))
         .withoutAuthentication()
 
     neo4jContainer.start()
     neo4jContainer.followOutput { println(it.utf8String) }
 
-    var driver = GraphDatabase.driver(neo4jContainer.getBoltUrl());
+    val driver = GraphDatabase.driver(neo4jContainer.getBoltUrl());
 
     return Pair(driver, neo4jContainer)
 }
@@ -109,12 +127,12 @@ fun startNeo() : Pair<Driver, Neo4jContainer<*>> {
 fun <T> writeInNeo(stmt: String, driver: Driver, extractor: (Result) -> T): T {
     return driver.session().use { session ->
         session.writeTransaction { tx ->
-//            val query = Query("CREATE (a:Greeting) SET a.message = \$message RETURN a.message + ', from node ' + id(a)", parameters("message", "testing"))
             val result = tx.run(stmt)
             extractor(result)
         }
     }
 }
+
 fun <T> readFromNeo(stmt: String, driver: Driver, extractor: (Result) -> T): T {
     return driver.session().use { session ->
         session.readTransaction() { tx ->
