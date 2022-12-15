48fb2a6fa15366c2c2cf01faac30848d6f734a32
Matthias Verbert <matthias@whitehare.be>
2022-12-15 10:52:10 +0100

    Day13

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -3,24 +3,57 @@ import kotlin.math.max
 
 fun main() {
     println("Day 13")
+    val inputfile = "day13/real.input"
+    val extrasFile = "day13/extra.input"
 
     var indexes = arrayListOf<Int>()
 
-    load("day13/real.input").readLines().windowed(2,3)
+    load(inputfile).readLines().windowed(2,3)
         .forEachIndexed { index, strings ->
             val left = strings[0]
             val right = strings[1]
-            println(left)
-            println(right)
-            val message = compare(packetOf(left), packetOf(right))
-            println("" + index + " = " + message)
-            println()
+//            println(left)
+//            println(right)
+            val message = comparePackets(packetOf(left), packetOf(right))
+//            println("" + index + " = " + message)
+//            println()
 
-            if (message == CompareResult.RIGHT_ORDER)
-                indexes.add(index+1)
+//            if (message == CompareResult.RIGHT_ORDER)
+//                indexes.add(index+1)
         }
 
-    println(indexes.sum())
+    val extras = load(extrasFile).readLines()
+        .map { packetOf(it) }
+
+    val originals = load(inputfile).readLines()
+        .filter { it.isNotBlank() }
+        .map { packetOf(it) }
+
+    val sorted = originals
+        .union(extras)
+        .sortedWith(Comparator())
+
+    sorted
+        .forEachIndexed { index, packet ->
+//            println(packet.pretty())
+            extras.forEach { extra ->
+                if (extra.pretty().equals(packet.pretty())) {
+                    indexes.add(index+1)
+                }
+            }
+        }
+
+    println(indexes)
+
+    println(indexes.fold(1) { a, b -> a * b })
+
+}
+
+class Comparator(): java.util.Comparator<Packet> {
+    override fun compare(o1: Packet?, o2: Packet?): Int {
+        return comparePackets(o1!!, o2!!).value
+    }
+
 }
 
 class Packet(val values: List<Packet>?, val value: Int?) {
@@ -31,6 +64,20 @@ class Packet(val values: List<Packet>?, val value: Int?) {
     fun hasValue(): Boolean {
         return value != null;
     }
+
+    override fun toString(): String {
+        return "Packet(values=$values, value=$value)"
+    }
+
+    fun pretty(): String {
+        if (hasValue()) {
+            return value.toString()
+        } else {
+            return "[" + values!!.joinToString(",") { it.pretty() } + "]"
+        }
+    }
+
+
 }
 
 fun packetOf(input: String): Packet {
@@ -47,38 +94,44 @@ fun packetOf(input: String): Packet {
     }
 }
 
-fun compare(left: Packet, right: Packet): CompareResult {
+fun comparePackets(left: Packet, right: Packet): CompareResult {
     if (left.hasValue() && right.hasValue()) {
-        return compare(left.value!!, right.value!!);
+        return comparePackets(left.value!!, right.value!!);
     } else if (left.hasValue()) {
-        return compare(listOf(left), right.values!!)
+        return comparePackets(listOf(left), right.values!!)
     } else if (right.hasValue()) {
-        return compare(left.values!!, listOf(right))
+        return comparePackets(left.values!!, listOf(right))
     } else {
-        return compare(left.values!!, right.values!!);
+        return comparePackets(left.values!!, right.values!!);
     }
 
 }
 
-fun compare(left: Int, right: Int): CompareResult {
+fun comparePackets(left: Int, right: Int): CompareResult {
+//    println("Compare $left vs $right")
     return if (left == right) {
         CompareResult.DUNNO
     } else if (left < right) {
+//        println("left < right")
         CompareResult.RIGHT_ORDER
     } else {
+//        println("left > right")
         CompareResult.WRONG_ORDER
     }
 }
-fun compare(left: List<Packet>, right: List<Packet>): CompareResult {
-    (0..max(left.size, right.size)).forEach {
+fun comparePackets(left: List<Packet>, right: List<Packet>): CompareResult {
+//    println("Compare $left vs $right")
+    (0 until max(left.size, right.size)).forEach {
         if (left.size <= it) {
+//            println("left ran out of items first")
             return CompareResult.RIGHT_ORDER // left ran out of items first
         } else if (right.size <= it) {
+//            println("right ran out of items first")
             return CompareResult.WRONG_ORDER // right ran out of items first
         } else {
             val leftVal = left.get(it)
             val rightVal = right.get(it)
-            val compareResult = compare(leftVal, rightVal)
+            val compareResult = comparePackets(leftVal, rightVal)
             if (compareResult !== CompareResult.DUNNO) { // left and right item are different
                 return compareResult
             }
@@ -88,9 +141,9 @@ fun compare(left: List<Packet>, right: List<Packet>): CompareResult {
 }
 
 
-enum class CompareResult {
-    RIGHT_ORDER,
-    WRONG_ORDER,
-    DUNNO
+enum class CompareResult(val value: Int) {
+    RIGHT_ORDER(-1),
+    WRONG_ORDER(1),
+    DUNNO(0)
 }
 
